5c58521c981fd0daef697e672a0aafc4f835f26c
Arden Feldt <feldt@ad.unc.edu>
2025-01-26 22:41:42 +0000

    im better at this rust than the other one fs

---
+++ hello_comp423/target/release/deps/hello_comp423-4d31b865e7c85dca.d
@@ -0,0 +1,5 @@
+/workspaces/rust-setup-proj/hello_comp423/target/release/deps/hello_comp423-4d31b865e7c85dca: src/main.rs
+
+/workspaces/rust-setup-proj/hello_comp423/target/release/deps/hello_comp423-4d31b865e7c85dca.d: src/main.rs
+
+src/main.rs:
---
+++ hello_comp423/target/release/hello_comp423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-setup-proj/hello_comp423/target/release/hello_comp423: /workspaces/rust-setup-proj/hello_comp423/src/main.rs
